93a4a654b88b5f0bbfc0a755f981f2a129d97ca7
amaljithUid <amaljith82@gmail.com>
2016-03-19 14:54:49 +0530

    Search Icon
    Search Icon

--- searchform.php
+++ searchform.php
@@ -2,4 +2,5 @@
 <form role="search" method="get" id="searchform" onSubmit="return title_validate()" name="f1" class="search hidden-sm hidden-xs" action="<?php echo esc_url( home_url( '' ) ); ?>/">
 		<input type="text" value="<?php echo get_search_query(); ?>" role="search"  name="s" id="s" class="search-box" placeholder="Search" />
 		<input type="hidden" name="search" value="s" />
+		<button type="submit"><i class="fa fa-search"></i></button>
 </form>
